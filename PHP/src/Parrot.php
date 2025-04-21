996771aa8efcb37dc464ebd3e577ce885f7ff13f
Andy Wendt <andy@awendt.com>
2025-04-21 12:17:38 -0600

    Remove NullParrot

--- PHP/src/Parrot.php
+++ PHP/src/Parrot.php
@@ -8,55 +8,27 @@ use Exception;
 
 class Parrot implements ParrotInterface
 {
-    private $instance;
-
     public static function instance(int $type, int $numberOfCoconuts, float $voltage, bool $isNailed)
     {
         return match ($type) {
             ParrotTypeEnum::EUROPEAN => new EuropeanParrot(),
             ParrotTypeEnum::AFRICAN => new AfricanParrot($numberOfCoconuts),
             ParrotTypeEnum::NORWEGIAN_BLUE => new NorwegianBlueParrot($voltage, $isNailed),
-            default => new NullParrot()
+            default => new self(),
         };
     }
-    
-    public function __construct(
-        /**
-         * @var int ParrotTypeEnum
-         */
-        private int $type,
-        private int $numberOfCoconuts,
-        private float $voltage,
-        private bool $isNailed
-    ) {
-        $this->instance = self::instance($type, $this->numberOfCoconuts, $this->voltage, $this->isNailed);
-    }
 
     /**
      * @throws Exception
      */
-    public function getSpeed(): float
-    {
-        return $this->instance->getSpeed();
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function getCry(): string
-    {
-        return $this->instance->getCry();
-    }
-}
-
-class NullParrot implements ParrotInterface
-{
-
     public function getSpeed(): float
     {
         throw new Exception('Should be unreachable');
     }
 
+    /**
+     * @throws Exception
+     */
     public function getCry(): string
     {
         return throw new Exception('Should be unreachable');
