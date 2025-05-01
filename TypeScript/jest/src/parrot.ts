5d2115c75796b79e2110c6006682c36033997b88
Andy Wendt <andy.wendt@sportsrecruits.com>
2025-05-01 08:22:50 -0600

    Add switch for creating parrots

--- TypeScript/jest/src/parrot.ts
+++ TypeScript/jest/src/parrot.ts
@@ -9,8 +9,16 @@ export class Parrot {
                                  numberOfCoconuts: number,
                                  voltage: number,
                                  isNailed: boolean) {
-
-        return new Parrot(parrotType, numberOfCoconuts, voltage, isNailed);
+        switch (parrotType) {
+            case ParrotTypes.EUROPEAN:
+                return new Parrot(parrotType, numberOfCoconuts, voltage, isNailed);
+            case ParrotTypes.AFRICAN:
+                return new Parrot(parrotType, numberOfCoconuts, voltage, isNailed);
+            case ParrotTypes.NORWEGIAN_BLUE:
+                return new Parrot(parrotType, numberOfCoconuts, voltage, isNailed);
+            default:
+                throw new Error("Should be unreachable");
+        }
     }
 
     constructor(private parrotType: ParrotTypes,
